c9c8cca8863b4e10353b96154498127cc2ae3968
Incognito-Sudo <gfranco.mg@gmail.com>
2022-10-01 21:15:59 -0700

    update RenderCampsite.js

---
+++ screens/CampsiteInfoScreen.js
@@ -0,0 +1,8 @@
+//import RenderCa
+
+const CampsiteInfoScreen = (props) => {
+    //it is using RenderCampsite to pass a prop
+    return <RenderCampsite campsite={props.campsite} /> //need to create a file for this
+}
+
+export default CampsiteInfoScreen;
